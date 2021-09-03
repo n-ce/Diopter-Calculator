2f985a6d7ca0a3852add2c2467bedd19db7daa30
Enceladus <69345507+n-ce@users.noreply.github.com>
2021-09-03 15:41:36 +0000

    #1 - replaced b with output tag, removed js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.body.oninput = function()
-{
-  var y = document.getElementById('y').value;
-  var x = document.getElementById('x').value;
-  document.querySelector('b').innerHTML=(y/(1+(y*(x/1000)))).toFixed(2);
-      }
